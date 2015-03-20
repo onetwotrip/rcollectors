448cd0555f65b8ede2f927b2618a83a0ea5e294a
Tony Nyurkin <ptqa.mail@gmail.com>
2015-03-20 12:40:42 +0300

    use one file

--- redis.rb
+++ redis.rb
@@ -3,6 +3,22 @@ require 'redis'
 require 'yaml'
 require_relative 'common.rb'
 
+def symbolize_keys(hash)
+  hash.inject({}){|result, (key, value)|
+    new_key = case key
+              when String then key.to_sym
+              else key
+              end
+    new_value = case value
+                when Hash then symbolize_keys(value)
+                when Array then value.map{ |v| v.is_a?(Hash) ? symbolize_keys(v) : v }
+                else value
+                end
+    result[new_key] = new_value
+    result
+  }
+end
+
 def load_config()
   return symbolize_keys(YAML.load_file('/etc/scollector/collectors/config.yml'))
 end
