0c93e59a3243ec5e7e371179771e5b4e8d551c61
Tony Nyurkin <ptqa.mail@gmail.com>
2015-05-20 16:09:01 +0300

    refactor

--- redis.rb
+++ redis.rb
@@ -1,34 +1,14 @@
 #!/usr/bin/env ruby
 require 'redis'
-require 'yaml'
+require_relative '../config'
 
-def symbolize_keys(hash)
-  hash.inject({}){|result, (key, value)|
-    new_key = case key
-              when String then key.to_sym
-              else key
-              end
-    new_value = case value
-                when Hash then symbolize_keys(value)
-                when Array then value.map{ |v| v.is_a?(Hash) ? symbolize_keys(v) : v }
-                else value
-                end
-    result[new_key] = new_value
-    result
-  }
-end
-
-def load_config()
-  return symbolize_keys(YAML.load_file('/etc/scollector/collectors/config.yml'))
-end
-
-def main()
-  config = load_config()[:redis]
+def main
+  config = load_config[:redis]
   redis_host = config[:host] || '127.0.0.1'
   redis_port = config[:port] || '6379'
   redis_conn = Redis.new(host: redis_host, port: redis_port)
   config[:metrics].each do |metric|
-    metric_value = redis_conn.send(metric[:cmd],metric[:args])
+    metric_value = redis_conn.send(metric[:cmd], metric[:args])
     taglist = ''
     metric[:tags].each do |tag,value|
       taglist += "#{tag}=#{value} "
@@ -37,4 +17,4 @@ def main()
   end
 end
 
-main()
+main
